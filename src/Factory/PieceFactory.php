eaab55bc072725a1104d58a6e7bd3b6f1e881a0c
TomMbn <tom.mauboussin@icloud.com>
2026-05-27 09:26:35 +0200

    Add PieceFactory

---
+++ src/Factory/PieceFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+class PieceFactory
+{
+    public function create(PieceType $type, PieceColor $color, Position $position): Piece
+    {
+        return match ($type) {
+            PieceType::KING   => new King($color, $position),
+            PieceType::QUEEN  => new Queen($color, $position),
+            PieceType::ROOK   => new Rook($color, $position),
+            PieceType::BISHOP => new Bishop($color, $position),
+            PieceType::KNIGHT => new Knight($color, $position),
+            PieceType::PAWN   => new Pawn($color, $position),
+        };
+    }
+}
